e819a8b97e584f11001001c0f948e4a4e2cfef08
Chandra Shekar Suryadevara <128857515+Chandra-Suryadevara@users.noreply.github.com>
2024-04-24 19:14:27 -0600

    made changes to make file

--- Src/Main.cpp
+++ Src/Main.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <random>
 #include "../Headers/Graphics_designer.hpp"
+#include "../Headers/Square.hpp"
 #include <SFML/Graphics.hpp>
 
 int main() {
     Graphics_designer Model;
+    Square main_char;
     
     Model.run();
+    main_char.draw(Model.get_windows());
 
     return 0;
 }
